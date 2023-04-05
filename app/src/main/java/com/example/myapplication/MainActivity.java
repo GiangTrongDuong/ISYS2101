855474a2a6cc1dd9c2eb8e4b33428c1ee926ab53
Giang Trong Duong <trongduong04112003@gmail.com>
2023-04-05 17:35:36 +0700

    Uploading to Test Branch new reader page
    Testing new reader using Parser

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button viewNews = findViewById(R.id.btnNews);
+        viewNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNews();
+            }
+        });
+    }
+
+    public void openNews(){
+        Intent intent = new Intent(this, NewsPages.class);
+        startActivity(intent);
     }
 }
