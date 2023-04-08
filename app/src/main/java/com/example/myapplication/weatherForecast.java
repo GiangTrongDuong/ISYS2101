5e27eccd27803d740c7cb6d034b905034c4815bd
Giang Trong Duong <trongduong04112003@gmail.com>
2023-04-08 12:18:16 +0700

    Finishing weather forecast guessing

--- app/src/main/java/com/example/myapplication/weatherForecast.java
+++ app/src/main/java/com/example/myapplication/weatherForecast.java
@@ -10,17 +10,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.URL;
 import java.text.DecimalFormat;
 
 public class weatherForecast extends AppCompatActivity {
 
-    private EditText cityName;
-    private TextView resultWeather;
+    EditText cityName;
+    TextView resultWeather;
     private final String url = "http://api.openweathermap.org/data/2.5/weather";
     private final String apiKey = "eb1a770f8e3624d1aebc453838659acd";
     DecimalFormat df = new DecimalFormat("#.##");
@@ -45,6 +51,32 @@ public class weatherForecast extends AppCompatActivity {
                 @Override
                 public void onResponse(String response) {
                     Log.d("response", response);
+                    String output = "";
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                        JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                        String description = jsonObjectWeather.getString("description");
+                            Log.d("JSONReturn", description);
+                        JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                        double temp = jsonObjectMain.getDouble("temp") - 273.15;
+                            Log.d("JSONReturn", String.valueOf(temp));
+                        double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
+                        int humidity = jsonObjectMain.getInt("humidity");
+                        JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                        String wind = jsonObjectWind.getString("speed");
+                        JSONObject jsonObjectCloud = jsonResponse.getJSONObject("clouds");
+                        String clouds = jsonObjectCloud.getString("all");
+                            Log.d("JSONReturn", clouds);
+                        JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
+                        String countryName = jsonObjectSys.getString("country");
+                        String countryCity = jsonResponse.getString("name");
+//                            Log.d("JSONReturn", cityNameString);
+                        output += "Current weather of " + countryCity + " ," + countryName + " is: " + df.format(temp) + "°C";
+                        resultWeather.setText(output);
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }, new Response.ErrorListener() {
                 @Override
@@ -52,6 +84,8 @@ public class weatherForecast extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
                 }
             });
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
         }
     }
 }
