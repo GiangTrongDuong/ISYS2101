a0ac5ad3a066041812c553cc48856baf622f6386
MrToilet <chitoan.tran@yahoo.com.vn>
2023-04-23 22:04:38 +0700

    Add Tools page

---
+++ app/src/main/java/com/example/tripme/TripToolsFragment.java
@@ -0,0 +1,60 @@
+package com.example.tripme;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.tripme.databinding.FragmentChecklistBinding;
+import com.example.tripme.databinding.FragmentTripToolsBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class TripToolsFragment extends Fragment {
+    Button buttonLogout, buttonNews, buttonWeather, buttonMap;
+    private FragmentTripToolsBinding binding;
+    public TripToolsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_trip_tools, container, false);
+        binding = FragmentTripToolsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        buttonNews = view.findViewById(R.id.box1);
+        buttonNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Go to the page
+            }
+        });
+        buttonWeather = view.findViewById(R.id.box2);
+        buttonWeather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Go to the page
+            }
+        });
+        buttonMap = view.findViewById(R.id.box3);
+        buttonMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Go to the page
+            }
+        });
+
+
+//        buttonLogout = view.findViewById(R.id.buttonLogout);
+//        buttonLogout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FirebaseAuth.getInstance().signOut();
+//                getActivity().finish();
+//            }
+//        });
+        return view;
+    }
+}
